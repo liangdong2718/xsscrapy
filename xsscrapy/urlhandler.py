5c5fe5b4b60d78bd3ce862e561932260957213ac
gxustudent <liangdong46@gmail.com>
2016-02-14 18:27:24 +0800

    1.add urlhandler.py to handler dup url

---
+++ xsscrapy/urlhandler.py
@@ -0,0 +1,31 @@
+import pdb
+import json
+from urlparse import urlparse, parse_qs, parse_qsl
+
+def obtain_key(url):
+  keys = []
+  parse_result = urlparse(url);
+  if parse_result.query == "":
+    return url
+  res_list = parse_qsl(parse_result.query)
+  for res in res_list:
+    keys.append(res[0])
+  keys.sort()
+  new_query = ""
+  for key in keys:
+    new_query += key + "&"
+  new_url = "http://" + parse_result.netloc + parse_result.path \
+        + "?" + new_query
+  print "new_url:"+new_url
+  return new_url
+
+def has_query(url):
+  parse_result = urlparse(url);
+  if parse_result.query == "":
+    return False
+  else:
+    return True
+
+if __name__ == '__main__':
+  test_url = "http://www2.shengda.edu.cn/ytw/tuanxiao/link.asp?action=go&fl_id=12"
+  obtain_key(test_url)
